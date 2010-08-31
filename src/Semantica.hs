731f855d1095f6ea0f88ad5c3aa177f019b45505
Daniel <daniel@daniel-laptop.(none)>
2010-08-31 10:31:13 -0500

    ...

--- src/Semantica.hs
+++ src/Semantica.hs
@@ -89,22 +89,3 @@ reduccion t = let t' = eval t
               in if t == t'
                  then t'
                  else reduccion t'
-
---Función para ver si hay cambio de signo en el intervalo
-signo f x y 
-          |(reduccion (FMult (sust f ('x',x)) (sust f ('x',y)))) < (FConst 0) = True
-          | otherwise = False                                                             
-
--- Función que me dice si es raiz
-raiz :: Func -> Func -> Bool         
-raiz f x 
-     | reduccion (sust f ('x',x)) == FConst 0  = True
-     | otherwise = False
-
--- Función que realiza la búsqueda incremental de un intervalo que contenga almenos 1 raiz
-busqdIncremental :: Func -> Func -> Func -> Integer -> [(Func,Func)]
-busqdIncremental f a d 0 = []
-busqdIncremental f a d i 
-    | (raiz f a /= False) = [((reduccion a),(reduccion a))] 
-    | (signo f a (FSum a d) /= False) = [((reduccion a),(reduccion(FSum a d)))] 
-    | (otherwise) = (busqdIncremental f (FSum a d) d (i-1))
