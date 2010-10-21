727d6b9e21b06c77bdd0ac4cef77d73f00697adc
Daniel Múnera Sánchez <dmunera119@gmail.com>
2010-10-21 00:19:49 -0500

    gaussiana para matrizAu

--- src/SistemasEcuaciones.hs
+++ src/SistemasEcuaciones.hs
@@ -10,6 +10,9 @@ leerMatriz n elem = listArray ((1,1),(n,n)) elem
 leerMatrizAu :: Integer -> [Double] -> Matriz
 leerMatrizAu n elem = listArray ((1,1),(n,n+1)) elem
 
+leerB :: Integer -> [Double] -> Matriz
+leerB n elem = listArray ((1,1),(1,n)) elem
+
 
 {-FUNCIONES AUXILIARES PARA PIVOTEOS -}
 
@@ -81,18 +84,18 @@ multFila m k f =  snd(head(filter (\x -> fst(fst x) == f) (multsEtapa m k)))
 
 {-Funcion que devuelve la submatriz que sera procesada dependiendo de la etapa k en la que va el proceso-}
 operm :: Matriz -> Integer -> Matriz
-operm a k = leerMatriz (n-(k-1)) (map (\x -> snd x)(filter (\x -> (snd(fst(x)) >= k && (fst(fst(x))) >= k)) (assocs a)))
+operm a k = leerMatrizAu (n-k) (map (\x -> snd x)(filter (\x -> (snd(fst(x)) >= k && (fst(fst(x))) >= k)) (assocs a)))
             where n = (snd(snd(bounds a)))
 
 {-Funcion que da a cada nuevo valor su respectivo indice para ser ubicado en la matriz A-}
 indOper :: Matriz -> Matriz -> Integer -> Matriz
-indOper a om k = listArray (((k+1),k), (n,n)) (elems om)
+indOper a om k = listArray (((k+1),k), ((n-1),n)) (elems om)
                  where n = snd(snd(bounds a))
 
 {-Funcion que actualiza la matriz A con sus nuevos valores -}
 actualizar :: Matriz -> Matriz -> Matriz
-actualizar a e = leerMatriz n (map (\x -> snd x)(map (\x -> (act' x (assocs e)) ) (assocs a)))
-                  where n = snd(snd(bounds a))
+actualizar a e = leerMatrizAu n (map (\x -> snd x)(map (\x -> (act' x (assocs e)) ) (assocs a)))
+                  where n = fst(snd(bounds a))
 
 {-Funcion auxiliar que se encarga de sustituir un valor viejo por uno nuevo en la matriz A -}
 act' :: ((Integer,Integer),Double) -> [((Integer,Integer),Double)] -> ((Integer,Integer),Double) 
@@ -103,8 +106,8 @@ act' b e
 
 {- ELIMINACION GAUSIANA-}
 
-eliminacionGausiana :: Matriz -> Integer -> Matriz
-eliminacionGausiana a n = etapak a n (n-1)
+eliminacionGaussiana :: Matriz -> Integer -> Matriz
+eliminacionGaussiana a n = etapak a n (n-1)
 
 etapak :: Matriz -> Integer -> Integer -> Matriz
 etapak a n 1 = actualizar a (indOper a (etapaj (operm a 1) (n-1))1)
@@ -112,7 +115,7 @@ etapak a n k = actualizar (etapak a n (k-1)) (indOper (etapak a n (k-1)) (etapaj
 
 {-Funcion que se encarga de las transformaciones de fila en cada etapa k, utiliza la funcion etapaj' que a su vez utiliza la funcion etapaj''-}
 etapaj :: Matriz -> Integer -> Matriz 
-etapaj a j = listArray ((1,1),(j,(j+1))) (map (\x -> snd x) (etapaj' a j))
+etapaj a j = listArray ((1,1),(j,(j+2))) (map (\x -> snd x) (etapaj' a j))
 
 etapaj' :: Matriz -> Integer -> [((Integer,Integer),Double)]
 etapaj' a 1  = etapaj'' a 1 
@@ -126,24 +129,20 @@ etapaj'' a j = (map (\x -> (((fst x), ((snd x) - (f x * mk))))) (darFila a (j+1)
 
 {-SUSTITUCIONES-}
 
---xn :: Matriz -> Integer -> [((Integer,Integer),Double)] -> Integer -> Double 
-xn a n b k = (i ,((head(map (\x -> snd x )(filter (\x -> (fst(fst(x))) == k) b))) / akk)) 
-                  where i = fst(numDiagonal (eliminacionGausiana a n) k)
-                        akk = snd(numDiagonal (eliminacionGausiana a n) k)
-suma :: [Double] -> Double
-suma [] = 0
-suma [a] = a
-suma (x:xs) = suma [x] + suma xs
-
+xn :: Matriz -> Integer -> Integer ->  Double
+xn a n k 
+       | k == n = (eliminacionGaussiana a n)!(n,(n+1)) /(eliminacionGaussiana a n)!(n,n)
+       | otherwise = (m!(k,k) - m!(k,k+1)*(xn a n (k+1)))/ m!(k,k)
+                     where m = eliminacionGaussiana a n
+                   
 
 {-PARA PRUEBAS-}
 m1 = leerMatriz 3 [2,1,4,3,2,3,6,1,4]
 m2 = leerMatriz 4 [20,1,3,2,4,60,-3,-7,1,2,50,7,-2,-7,4,18]
 m3 = leerMatriz 3 [10,2,5,3,12,2,-4,-5,15]
-m21 = etapak m2 4 1
-m22 = etapak m2 4 2
-b = [((1,1),1.0),((2,1),2.0),((3,1),3.0),((4,1),4.0)]
-b3 = [((1,1),1.0),((2,1),2.0),((3,1),3.0)]
+m4 = leerMatriz 4 [2,-3,10,-7,3,12,-16,16,14,-18,40,-7,16,-8,-50,6]
+m4au =  leerMatrizAu 4 [2,-3,10,-7,20,   3,12,-16,16,30  ,14,-18,40,-7, 25 ,16,-8,-50,6,-18]
+m5 = leerMatriz 4 [-7,2,-3,4,5,-1,14,-1,1,9,-7,5,-12,13,-8,-4]
 m2au =  leerMatrizAu 4 [20,1,3,2,1,  4,60,-3,-7,1,  1,2,50,7,1, -2,-7,4,18,1]
 
 
