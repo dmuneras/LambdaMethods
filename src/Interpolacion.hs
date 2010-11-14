08da73feffe971b9e0d524b37859f26c7c00ebfb
Daniel Múnera Sánchez <dmunera119@gmail.com>
2010-11-13 23:19:30 -0500

    correccion pivoteos

--- src/Interpolacion.hs
+++ src/Interpolacion.hs
@@ -146,6 +146,20 @@ srchValue x v = if (n1 == 0) then (n1+1) else (n1)
                 where n1 = length (fst mitd)
                       mitd = break (>=v) x
 
+
+{-FUNCIONES GENERALES DE SPLINES CUADRATICOS Y CUBICOS
+-}
+
+--Funcion que halla los terminos independientes de las ecuaciones de empalme. Basada en la practica referenciada
+empalB :: [Double] -> [Double] -> Int -> Int -> [Double]
+empalB fx l i n
+           | i<=(n-1) = empalB fx (l++[fx!!i,fx!!i]) (i+1) n
+           | otherwise = l
+
+--Funcion que halla los terminos independientes de las ecuaciones de los extremos. Basada en la practica referenciada
+extB :: [Double] -> Int -> [Double]
+extB fx n = [fx!!0,fx!!n]
+
 {-SPLINES CUADRATICOS
 NOTA: Este metodo tiene dos partes: Una que evalua un punto nuevo en el polinomio y da el resultado de la interpolacion y otra que 
 entrega el polinomio interpolante escrito por medio de nuestra gramatica de funciones. Esta basado en la implementacion hecha por 
@@ -154,7 +168,7 @@ Santiago Rodriguez y Carolina Campillo en la practica del semestre 2010-1.
 
 --Funcion que retorna los valores de los coeficientes
 coefSpCuad :: [Double] -> [Double] -> [(Integer,Double)]
-coefSpCuad x fx = eGaussSim (matrizSpCuad x fx) (fromIntegral (length x -1)*3)
+coefSpCuad x fx = eGaussPParcial (matrizSpCuad x fx) (fromIntegral (length x -1)*3)
 
 --Funcion que arma la matriz aumentada del sistema de ecuaciones
 matrizSpCuad :: [Double] -> [Double] -> Matriz
@@ -171,27 +185,17 @@ empalCuad :: [Double] -> [Double] -> [Double] -> Int -> Int -> Int -> [Double]
 empalCuad x fx l i n neq
           | i<=(n-1) = empalCuad x fx (l++(take ((i-1)*3) [0,0..])++[(x!!i)^2,x!!i,1]++(take (neq-3*i) [0,0..])++[b1]++(take (3*i) [0,00..])++[(x!!i)^2,x!!i,1]++(take (neq -3-(i)*3) [0,0..])++[b2]) (i+1) n neq
           | otherwise = l
-          where b = empalCuadB fx [] 1 n
+          where b = empalB fx [] 1 n
                 b1 = if (i==1) then (b!!(i-1)) else (b!!i)
                 b2 = if (i==1) then (b!!i) else (b!!(i+1))
-
---Funcion que halla los terminos independientes de las ecuaciones de empalme. Basada en la practica referenciada
-empalCuadB :: [Double] -> [Double] -> Int -> Int -> [Double]
-empalCuadB fx l i n
-           | i<=(n-1) = empalCuadB fx (l++[fx!!i,fx!!i]) (i+1) n
-           | otherwise = l
 
 --Funcion que halla las ecuaciones de los extremos. Basada en la practica referenciada
 extCuad :: [Double] -> [Double] -> Int -> Int -> [Double]
 extCuad x fx neq n = ([(x!!0)^2,x!!0,1]++(take zeros [0,0..])++[b!!0]++(take zeros [0,0..])++[(x!!n)^2,x!!n,1]++[b!!1])
                      where zeros = div (neq*2-6) 2
-                           b = extCuadB fx n
+                           b = extB fx n
 
---Funcion que halla los terminos independientes de las ecuaciones de los extremos. Basada en la practica referenciada
-extCuadB :: [Double] -> Int -> [Double]
-extCuadB fx n = [fx!!0,fx!!n]
-
---Funcion que halla las funciones de las derivadas en los nodos internos. Basada en la practica referenciada
+--Funcion que halla las funciones de las derivadas en los nodos internos y las iguala. Basada en la practica referenciada
 derCuad :: [Double] -> Int -> Int -> [Double] ->Int -> [Double]
 derCuad x n neq l i
         | i<=(n-1) = derCuad x n neq (l++(take ((i-1)*3) [0,0..])++[2*(x!!i),1,0,-2*(x!!i),-1,0]++(take ((neq -3 -i*3)+1) [0,0..])) (i+1)
@@ -200,6 +204,57 @@ derCuad x n neq l i
 --Funcion que halla la segunda derivada de la primera ecuacion igualada a cero. Basada  en la practica referenciada
 sdaDerCuad :: Int -> [Double]
 sdaDerCuad neq = [1]++(take neq [0,0..])
+
+{-SPLINES CUBICOS
+NOTA: Este metodo tiene dos partes: Una que evalua un punto nuevo en el polinomio y da el resultado de la interpolacion y otra que 
+entrega el polinomio interpolante escrito por medio de nuestra gramatica de funciones. Esta basado en la implementacion hecha por 
+Santiago Rodriguez y Carolina Campillo en la practica del semestre 2010-1.
+-}
+
+--Funcion que retorna los valores de los coeficientes
+coefSpCub :: [Double] -> [Double] -> [(Integer,Double)]
+coefSpCub x fx = eGaussPParcial (matrizSpCub x fx) (fromIntegral (length x -1)*4)
+
+--Funcion que arma la matriz aumentada del sistema de ecuaciones
+matrizSpCub :: [Double] -> [Double] -> Matriz
+matrizSpCub x fx = leerMatrizAu (fromIntegral (length x -1)* 4) (sistmEcSpCub x fx)
+
+--Funcion que une todas las ecuaciones del sistema en una lista sencilla. Basada en la practica referenciada
+sistmEcSpCub :: [Double] -> [Double] -> [Double]
+sistmEcSpCub x fx = (empalCub x fx [] 1 n neq)++(extCub x fx neq n)++(derCub x n neq [] 1)++(sdaDerCub x n neq [] 1)++(sdaDerExtCub x neq n)
+                    where n = length x -1
+                          neq  = (n)*4
+
+--Funcion que halla las ecuaciones de empalme. Basada en la practica referenciada
+empalCub :: [Double] -> [Double] -> [Double] -> Int -> Int -> Int -> [Double]
+empalCub x fx l i n neq
+         | i<=(n-1) = empalCub x fx (l++(take ((i-1)*4) [0,0..])++[(x!!i)^3,(x!!i)^2,x!!i,1]++(take (neq-4*i) [0,0..])++[b1]++(take (4*i) [0,0..])++[(x!!i)^3,(x!!i)^2,x!!i,1]++(take (neq -4-(i)*4) [0,0..])++[b2]) (i+1) n neq
+         | otherwise = l
+         where b = empalB fx [] 1 n
+               b1 = if (i==1) then (b!!(i-1)) else (b!!i)
+               b2 = if (i==1) then (b!!i) else (b!!(i+1))
+
+--Funcion que halla las ecuaciones de los extremos. Basada en la practica referenciada
+extCub :: [Double] -> [Double] -> Int -> Int -> [Double]
+extCub x fx neq n = ([(x!!0)^3,(x!!0)^2,x!!0,1]++(take zeros [0,0..])++[b!!0]++(take zeros [0,0..])++[(x!!n)^3,(x!!n)^2,x!!n,1]++[b!!1])
+                    where zeros = div (neq*2-8) 2
+                          b = extB fx n
+
+--Funcion que halla las funciones de las derivadas en los nodos internos y las iguala. Basada en la practica referenciada
+derCub :: [Double] -> Int -> Int -> [Double] -> Int -> [Double]
+derCub x n neq l i
+       | i<=(n-1) = derCub x n neq (l++(take ((i-1)*4) [0,0..])++[3*(x!!i)^2,2*(x!!i),1,0,-3*(x!!i)^2,-2*(x!!i),-1,0]++(take ((neq -4 -i*4)+1) [0,0..])) (i+1)
+       | otherwise = l
+
+--Funcion que halla las funciones de las segundas derivadas en los nodos internos y las iguala. Basada en la practica referenciada
+sdaDerCub :: [Double] -> Int -> Int -> [Double] -> Int -> [Double]
+sdaDerCub x n neq l i 
+          | i<=(n-1) = sdaDerCub x n neq (l++(take ((i-1)*4) [0,0..])++[6*(x!!i),2,0,0,-6*(x!!i),-2,0,0]++(take ((neq -4 -i*4)+1) [0,0..])) (i+1)
+          | otherwise = l
+
+--Funcion que halla las segundas derivadas de los extremos y las iguala a cero. Basada en la practica referenciada
+sdaDerExtCub :: [Double] -> Int -> Int -> [Double]
+sdaDerExtCub x neq n = [6*(x!!0),2,0,0]++(take ((neq*2 -8)+1) [0,0..])++[6*(x!!n),2,0,0,0]
 
 --Valores para prueba
 x = [2.3,2.4,2.5,2.6]
